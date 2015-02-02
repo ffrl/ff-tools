6d4c6a6f36bc783b3cf4548d80fd34dba1bfd336
CyrusFox <cyrus@lamdacore.de>
2015-02-02 18:29:39 +0100

    Added proper try/except for the socket as well

--- fastd/fastd-statistics.py
+++ fastd/fastd-statistics.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 import socket
 import sys
 import os
@@ -21,26 +21,25 @@ args = vars(parser.parse_args())
 print >>sys.stderr, 'Connecting to %s' % args['socket']
 
 client = socket.socket( socket.AF_UNIX, socket.SOCK_STREAM )
-client.connect(args['socket'])
-
-message = recv_data(client)
-
 try:
-    fastd_data = json.loads(message.decode('utf-8'))
-    print "=============Fastd Statistics============="
-    print "Fastd Uptime:", str(datetime.timedelta(milliseconds=int(fastd_data["uptime"])))
-    print "Connected Clients:", len(fastd_data["peers"])
-    print "RX packets:", fastd_data["statistics"]["rx"]["packets"]
-    print "RX bytes:", fastd_data["statistics"]["rx"]["bytes"]
-    print "RX reordered packets:", fastd_data["statistics"]["rx_reordered"]["packets"]
-    print "RX reordered bytes:", fastd_data["statistics"]["rx_reordered"]["bytes"]
-    print "TX packets:", fastd_data["statistics"]["tx"]["packets"]
-    print "TX bytes:", fastd_data["statistics"]["tx"]["bytes"]
-    print "TX dropped packets:", fastd_data["statistics"]["tx_dropped"]["packets"]
-    print "TX dropped bytes:", fastd_data["statistics"]["tx_dropped"]["bytes"]
-    print "TX error packets:", fastd_data["statistics"]["tx_error"]["packets"]
-    print "TX error bytes:", fastd_data["statistics"]["tx_error"]["bytes"]
-
-except (ValueError, KeyError, TypeError):
-    print "JSON format error"
-
+    client.connect(args['socket'])
+    message = recv_data(client)
+    try:
+        fastd_data = json.loads(message.decode('utf-8'))
+        print "=============Fastd Statistics============="
+        print "Fastd Uptime:", str(datetime.timedelta(milliseconds=int(fastd_data["uptime"])))
+        print "Connected Clients:", len(fastd_data["peers"])
+        print "RX packets:", fastd_data["statistics"]["rx"]["packets"]
+        print "RX bytes:", fastd_data["statistics"]["rx"]["bytes"]
+        print "RX reordered packets:", fastd_data["statistics"]["rx_reordered"]["packets"]
+        print "RX reordered bytes:", fastd_data["statistics"]["rx_reordered"]["bytes"]
+        print "TX packets:", fastd_data["statistics"]["tx"]["packets"]
+        print "TX bytes:", fastd_data["statistics"]["tx"]["bytes"]
+        print "TX dropped packets:", fastd_data["statistics"]["tx_dropped"]["packets"]
+        print "TX dropped bytes:", fastd_data["statistics"]["tx_dropped"]["bytes"]
+        print "TX error packets:", fastd_data["statistics"]["tx_error"]["packets"]
+        print "TX error bytes:", fastd_data["statistics"]["tx_error"]["bytes"]
+    except (ValueError, KeyError, TypeError):
+        print "JSON format error"
+except:
+    pass
